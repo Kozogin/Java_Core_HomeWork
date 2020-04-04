298e69882f8eee56d01bbd23bd440fec9962d741
LogosITAcademy <Tester99037@gmail.com>
2020-04-04 09:13:26 +0300

    end

--- Java_Core/src/ua/lviv/lgs/home20/ExecutorServicesT2.java
+++ Java_Core/src/ua/lviv/lgs/home20/ExecutorServicesT2.java
@@ -3,21 +3,22 @@ package ua.lviv.lgs.home20;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorServicesT2 {
 
-static int number;
+	static int number;
 	
 	public int getNumber() {
-		return number;		
+		return number;
 	}
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -26,6 +27,8 @@ static int number;
 		System.out.println("Enter number members Fibonacci");
 		Scanner sc = new Scanner(System.in);
 		number = sc.nextInt();
+		
+		int timer = number + 2;
 		
 		Callable<List<Integer>> createFibo = () -> {
 			List<Integer> fibo = new ArrayList<>();
@@ -43,8 +46,15 @@ static int number;
         Callable<List<Integer>> tasksList = createFibo;         
         Future<List<Integer>> results = executor.submit(tasksList); 
         
-        ExecutorService obverse = Executors.newSingleThreadExecutor();
+        while(!results.isDone())
+        {
+         Thread.sleep(1);
+        }        
+        executor.shutdown();
+                
+        //-----------------------------------------------------------------
         
+        ExecutorService obverse = Executors.newSingleThreadExecutor();        
         obverse.execute(new Runnable() {
 			
 			@Override
@@ -54,43 +64,40 @@ static int number;
 					while(iterator.hasNext()) {
 						Thread.sleep(1000);
 						System.out.print(iterator.next() + ", ");
-					}					
+					}	
+					System.out.println(); 
+					
 				} catch (InterruptedException | ExecutionException e) {
 					e.printStackTrace();
 				}				
 			}
-		});
-        System.out.println();
+		});              
         
         obverse.shutdown();
         
-        
-        Timer time = new Timer();
-        ReversTask revers = new ReversTask();
-        time.schedule(revers, 0, 1000); // 
-
-        for (int i = 0; i <= number; i++) {
-            Thread.sleep(10);  
-            System.out.println("Execution in Main Thread. #" + i);
-            if (i == 5) {
-                System.out.println("Application Terminates");
-                //System.exit(0);
-            }
-        }
+        //---------------------------------------------
         
         
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(4);
+      Iterator<Integer> iterator1 = results.get().listIterator();
+      while(iterator1.hasNext()) {
+    	  iterator1.next();
+      }
+      
+        Runnable revers = () -> {
+						
+			if(((ListIterator<Integer>) iterator1).hasPrevious()) {				
+				System.out.print(((ListIterator<Integer>) iterator1).previous() + ", ");
+			
+			} else {
+				System.out.println();
+				System.out.println("exit");
+				scheduledExecutorService.shutdown();
+				System.exit(0);
+			}
+		};
+		scheduledExecutorService.scheduleAtFixedRate(revers, timer, 1, TimeUnit.SECONDS);
         
-        
-        
-		
 	}
 }
 
-
-class ReversTask extends TimerTask {
-
-	@Override
-	public void run() {
-		System.out.println("dfgdhdhddjjdjjdjj");
-	}
-}
